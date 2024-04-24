a3889ec19bbc82be621c9e0735d0c4c2845a89f1
VCommsNetlify <167278407+VCommsNetlify@users.noreply.github.com>
2024-04-24 18:22:18 +0800

    Update microsite ver1.js

--- microsite ver1.js
+++ microsite ver1.js
@@ -321,7 +321,7 @@ function createThumbnails() {
         var thumbnail = document.createElement('img');
         if (h === 1) {
             thumbnail.src = 'Assets/3. Events, Gallery and Socials/3. News/04222024-our-associate-v-partners-invite-you-to-v-malaysia-20245db576f0-672a-4dfe-a987-d09849d39ff3.png';c
-		else if (h === 2) {
+		} else if (h === 2) {
 			thumbnail.src = 'Assets/3. Events, Gallery and Socials/3. News/04192024-vmalaysia2024-mission-unstoppable-vp-mahendra-kumar59b3cac6-4373-4f50-9fd7-8d9dec0bbfd3.png';
 		} else if (h === 3) {
 			thumbnail.src = 'Assets/3. Events, Gallery and Socials/3. News/04192024-vmalaysia2024-mission-unstoppable-vp-kalai-manikamaa2ea855-781e-4369-9f30-767432c244fepng';
