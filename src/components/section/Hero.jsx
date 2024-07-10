4d19dca3692dd04b84ccee7d82ba92a0971ac3b7
VCommsNetlify <167278407+VCommsNetlify@users.noreply.github.com>
2024-07-10 15:17:11 +0800

    Update Hero.jsx

--- src/components/section/Hero.jsx
+++ src/components/section/Hero.jsx
@@ -114,7 +114,7 @@ const Hero = () => {
                                 <a className="dropdown-item" href="#">
                                     English
                                 </a>
-                                <a className="dropdown-item" href="ar.html">
+                                <a className="dropdown-item" href="public/js/ar.html">
                                     العربية
                                 </a>
                                 <a className="dropdown-item" href="#">
