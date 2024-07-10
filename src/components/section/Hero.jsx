53753624a7d7605888e93bae0a029dcb7d1470f8
VCommsNetlify <167278407+VCommsNetlify@users.noreply.github.com>
2024-07-10 19:58:05 +0800

    Update Hero.jsx

--- src/components/section/Hero.jsx
+++ src/components/section/Hero.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { useTranslation } from "react-i18next";
-import '../i18n';
+import 'src/app/i18n.js';
 
 const Hero = () => {
     const { t } = useTranslation();
